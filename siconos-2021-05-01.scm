50448125c252e37ff3683b6c79153905a7a50952
Maurice Bremond <Maurice.Bremond@inria.fr>
2021-05-01 16:12:13 +0200

    [channels] add spec with siconos-tutorials

---
+++ siconos-2021-05-01.scm
@@ -0,0 +1,15 @@
+(list (channel
+        (name 'guix-siconos)
+        (url "https://gitlab.inria.fr/bremond/guix-siconos")
+        (commit
+          "c84182005b56ae00b4c6bb52d2ef10f6a2cd3ccc"))
+      (channel
+        (name 'guix)
+        (url "https://git.savannah.gnu.org/git/guix.git")
+        (commit
+          "45549e9f31e462d0d1519fd678dd782da6ed5cb9")
+        (introduction
+          (make-channel-introduction
+            "9edb3f66fd807b096b48283debdcddccfea34bad"
+            (openpgp-fingerprint
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
