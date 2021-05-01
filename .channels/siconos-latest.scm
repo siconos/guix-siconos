caa30e39bbda781657ad8d15fa40ff8205859e6a
Maurice Bremond <Maurice.Bremond@inria.fr>
2021-05-01 15:44:12 +0200

    [draft] clean channels specs

--- .channels/siconos-latest.scm
+++
@@ -1,7 +0,0 @@
-;; This is not a reproductible channels specification as there is no
-;; commit constraints on both guix-siconos and guix channels
-(cons
- (channel
-  (name 'guix-siconos)
-  (url "https://gitlab.inria.fr/bremond/guix-siconos"))
- %default-channels)
