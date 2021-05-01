2f1a70df67953aacebe834f1884fd19b6f096b30
Maurice Bremond <Maurice.Bremond@inria.fr>
2021-05-01 15:42:49 +0200

    [draft] add a channels branch with channels specs

---
+++ siconos-latest.scm
@@ -0,0 +1,7 @@
+;; This is not a reproductible channels specification as there is no
+;; commit constraints on both guix-siconos and guix channels
+(cons
+ (channel
+  (name 'guix-siconos)
+  (url "https://gitlab.inria.fr/bremond/guix-siconos"))
+ %default-channels)
