e9cfb6b02241bf30203f77422ffb1c1dfad9116d
Maurice Bremond <Maurice.Bremond@inria.fr>
2021-05-03 11:24:13 +0200

    [channels] add 2021-05-03

---
+++ siconos-2021-05-03.scm
@@ -0,0 +1,15 @@
+(list (channel
+        (name 'guix-siconos)
+        (url "https://gitlab.inria.fr/bremond/guix-siconos")
+        (commit
+          "be544bc07cd256f8e14566e0bfb3d59828ea4f0e"))
+      (channel
+        (name 'guix)
+        (url "https://git.savannah.gnu.org/git/guix.git")
+        (commit
+          "f0eb053dac8273416ab24541b85617d79dbae134")
+        (introduction
+          (make-channel-introduction
+            "9edb3f66fd807b096b48283debdcddccfea34bad"
+            (openpgp-fingerprint
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
