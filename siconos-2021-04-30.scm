988178fdb804d484a1d67dd80177dd67a8d0db4e
Maurice Bremond <Maurice.Bremond@inria.fr>
2021-05-04 15:31:39 +0200

    [channels] remove introduction field / compatibility with old guix

--- siconos-2021-04-30.scm
+++ siconos-2021-04-30.scm
@@ -7,9 +7,5 @@
        (name 'guix)
        (url "https://git.savannah.gnu.org/git/guix.git")
        (commit
-        "131265ea3a3d71c3b430bce6bb2d428cc1a2a4fa")
-       (introduction
-        (make-channel-introduction
-         "9edb3f66fd807b096b48283debdcddccfea34bad"
-         (openpgp-fingerprint
-          "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
+        "131265ea3a3d71c3b430bce6bb2d428cc1a2a4fa")))
+
--- siconos-2021-05-01.scm
+++ siconos-2021-05-01.scm
@@ -7,9 +7,4 @@
         (name 'guix)
         (url "https://git.savannah.gnu.org/git/guix.git")
         (commit
-          "45549e9f31e462d0d1519fd678dd782da6ed5cb9")
-        (introduction
-          (make-channel-introduction
-            "9edb3f66fd807b096b48283debdcddccfea34bad"
-            (openpgp-fingerprint
-              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
+          "45549e9f31e462d0d1519fd678dd782da6ed5cb9")))
