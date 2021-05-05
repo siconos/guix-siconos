870a985b7b424e3d2f7741b19b6d36bbb0a5ae47
Maurice Bremond <Maurice.Bremond@inria.fr>
2021-05-05 10:27:29 +0200

    [channels] add channel for improved packages definitions

---
+++ siconos-2021-05-05.scm
@@ -0,0 +1,10 @@
+(list (channel
+        (name 'guix-siconos)
+        (url "https://gitlab.inria.fr/bremond/guix-siconos")
+        (commit
+          "9b9e05c477259de69f350620fc1b278a5f698136"))
+      (channel
+        (name 'guix)
+        (url "https://git.savannah.gnu.org/git/guix.git")
+        (commit
+          "0f116d7f909b6c1f1cc95c8b07d688b697d229b1")))
