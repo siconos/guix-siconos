c502e739a314e9c9f658724dceecaa95a9b36140
Maurice Bremond <Maurice.Bremond@inria.fr>
2021-05-10 11:39:34 +0200

    [channels] add first check on github

---
+++ siconos-2021-05-10.scm
@@ -0,0 +1,10 @@
+(list (channel
+        (name 'guix-siconos)
+        (url "https://github.com/siconos/guix-siconos")
+        (commit
+          "b30c74ee0e14418c1575f784833e048dec31fb10"))
+      (channel
+        (name 'guix)
+        (url "https://git.savannah.gnu.org/git/guix.git")
+        (commit
+          "ce3d05cc08c01351756ab5d5b7f25cfe0295c230")))
