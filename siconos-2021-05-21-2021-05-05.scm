040e8bd895061c1f65d2f71095566b0ac981e653
Maurice Bremond <Maurice.Bremond@inria.fr>
2021-05-21 17:45:51 +0200

    [channels] add spec with different dates for siconos & guix

---
+++ siconos-2021-05-21-2021-05-05.scm
@@ -0,0 +1,10 @@
+(list (channel
+       (name 'guix-siconos)
+       (url "https://github.com/siconos/guix-siconos")
+       (commit
+        "e8691f7b2999ecb617d1d3dba244d9f1b3e6377c"))
+      (channel
+       (name 'guix)
+       (url "https://git.savannah.gnu.org/git/guix.git")
+       (commit
+        "0f116d7f909b6c1f1cc95c8b07d688b697d229b1")))
