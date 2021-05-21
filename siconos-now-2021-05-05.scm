0c8193621f26746423f27ccb6130619d6bc2c77e
Maurice Bremond <Maurice.Bremond@inria.fr>
2021-05-21 10:52:40 +0200

    [channels] add fixed guix + unfixed siconos

---
+++ siconos-now-2021-05-05.scm
@@ -0,0 +1,8 @@
+(list (channel
+       (name 'guix-siconos)
+       (url "https://gitlab.inria.fr/bremond/guix-siconos"))
+      (channel
+       (name 'guix)
+       (url "https://git.savannah.gnu.org/git/guix.git")
+       (commit
+        "0f116d7f909b6c1f1cc95c8b07d688b697d229b1")))
