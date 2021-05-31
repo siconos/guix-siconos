0fafa133f142bde0df2d0b1477e7a8220b3c3897
Maurice Bremond <Maurice.Bremond@inria.fr>
2021-05-31 16:29:03 +0200

    [channels] occ examples / oldest guix

---
+++ siconos-2021-05-31-2021-05-10.scm
@@ -0,0 +1,10 @@
+(list (channel
+        (name 'guix)
+        (url "https://git.savannah.gnu.org/git/guix.git")
+        (commit
+          "ce3d05cc08c01351756ab5d5b7f25cfe0295c230"))
+      (channel
+        (name 'guix-siconos)
+        (url "https://github.com/siconos/guix-siconos")
+        (commit
+          "68ee2eb947ca941f4feeda6d5aa76a0311aa8ffe")))
