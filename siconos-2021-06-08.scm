7809fe6ad90b89f3d6d444c00111a858fc0a2dcb
Maurice Bremond <Maurice.Bremond@inria.fr>
2021-06-08 17:53:14 +0200

    [channels] today channel, ok with stress test & optimize 2d granular

---
+++ siconos-2021-06-08.scm
@@ -0,0 +1,10 @@
+(list (channel
+       (name 'guix)
+       (url "https://git.savannah.gnu.org/git/guix.git")
+       (commit
+        "503c2039a280dd52a751a6852b4157fccd1b4195"))
+      (channel
+       (name 'guix-siconos)
+       (url "https://github.com/siconos/guix-siconos")
+       (commit
+        "c4c3dcbef8700b98290d5fa5f5b606b32e8096fe")))
