f69203b1f24da3ee5c75b9579b389e141a2d69a5
Maurice Bremond <Maurice.Bremond@inria.fr>
2022-04-07 10:42:47 +0200

    [channels] tested with cxd benchmarks

---
+++ siconos-2022-04-06.scm
@@ -0,0 +1,17 @@
+(list (channel
+        (name 'guix)
+        (url "https://git.savannah.gnu.org/git/guix.git")
+        (branch "master")
+        (commit
+          "3b3cc9dfed30afec70a6feaced3710bf79f8b2bc")
+        (introduction
+          (make-channel-introduction
+            "9edb3f66fd807b096b48283debdcddccfea34bad"
+            (openpgp-fingerprint
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+      (channel
+        (name 'guix-siconos)
+        (url "https://github.com/siconos/guix-siconos")
+        (branch "master")
+        (commit
+          "59be60e1f9f37935a5e46cc2e5e987f6f49ede05")))
