f1b5d401e0661a1b7d57271a1095178a323e6ffa
Maurice Bremond <Maurice.Bremond@inria.fr>
2023-05-10 09:53:11 +0200

    [channels] update 2023

---
+++ siconos-2023-03-06.scm
@@ -0,0 +1,17 @@
+(list (channel
+        (name 'guix)
+        (url "https://git.savannah.gnu.org/git/guix.git")
+        (branch "master")
+        (commit
+          "461577f0fce1b69a88a752857eeee2e9e1116d6c")
+        (introduction
+          (make-channel-introduction
+            "9edb3f66fd807b096b48283debdcddccfea34bad"
+            (openpgp-fingerprint
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+      (channel
+        (name 'guix-hpc)
+        (url "https://gitlab.inria.fr/bremond/guix-hpc.git")
+        (branch "add-siconos")
+        (commit
+          "05b87e068001a7b00aa3b61eb21c89f83335c116")))
