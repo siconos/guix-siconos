0ac2acc63e5492a5777d65b56de3c305aa131714
Maurice Bremond <Maurice.Bremond@inria.fr>
2023-05-31 15:52:41 +0200

    [channels] update with guix-hpc/add-siconos

--- siconos-2023-03-06.scm
+++ siconos-2023-03-06.scm
@@ -1,4 +1,10 @@
 (list (channel
+        (name 'guix-hpc)
+        (url "https://gitlab.inria.fr/bremond/guix-hpc.git")
+        (branch "add-siconos")
+        (commit
+          "1afe0499a94ca3e02f43631272e36c6755784a2a"))
+      (channel
         (name 'guix)
         (url "https://git.savannah.gnu.org/git/guix.git")
         (branch "master")
@@ -8,10 +14,4 @@
           (make-channel-introduction
             "9edb3f66fd807b096b48283debdcddccfea34bad"
             (openpgp-fingerprint
-              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
-      (channel
-        (name 'guix-hpc)
-        (url "https://gitlab.inria.fr/bremond/guix-hpc.git")
-        (branch "add-siconos")
-        (commit
-          "05b87e068001a7b00aa3b61eb21c89f83335c116")))
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
