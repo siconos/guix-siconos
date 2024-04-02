df5a7e1f397e95623207b2f9d71926cd2d2f15c8
Maurice Bremond <Maurice.Bremond@inria.fr>
2024-04-02 16:02:44 +0200

    [channels] add 3/2024

---
+++ siconos-2024-03-17.scm
@@ -0,0 +1,17 @@
+(list (channel
+        (name 'guix)
+        (url "https://git.savannah.gnu.org/git/guix.git")
+        (branch "master")
+        (commit
+          "d1b6d77aaef3c2a7ecc930de1cce2b9ac20203bf")
+        (introduction
+          (make-channel-introduction
+            "9edb3f66fd807b096b48283debdcddccfea34bad"
+            (openpgp-fingerprint
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+      (channel
+        (name 'guix-hpc)
+        (url "https://gitlab.inria.fr/bremond/guix-hpc.git")
+        (branch "add-siconosx")
+        (commit
+          "2a670fc1d033359e13afac8a575c0b66b2093781")))
