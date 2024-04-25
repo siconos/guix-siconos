0e5f7bcca3d7ba64c19f0950ed14f0eec3cfce4c
Maurice Bremond <Maurice.Bremond@inria.fr>
2024-04-25 11:38:07 +0200

    [channels] update / install pythonocc-core

---
+++ siconos-2024-04-25.scm
@@ -0,0 +1,17 @@
+(list (channel
+        (name 'guix)
+        (url "https://git.savannah.gnu.org/git/guix.git")
+        (branch "master")
+        (commit
+          "4514239971895fc7ad47c7752e30d98a0c44a154")
+        (introduction
+          (make-channel-introduction
+            "9edb3f66fd807b096b48283debdcddccfea34bad"
+            (openpgp-fingerprint
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+      (channel
+        (name 'guix-hpc)
+        (url "https://gitlab.inria.fr/bremond/guix-hpc.git")
+        (branch "add-siconosx")
+        (commit
+          "7ac06c2ce536907a1f2d03ce9787fd8f548f8a6b")))
